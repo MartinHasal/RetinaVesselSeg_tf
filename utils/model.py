4e0e3dd11485727e3479d5fa861ab54d3f55986d
MartinHasal <m.hasal@email.cz>
2023-04-07 12:30:53 +0200

    save option to job:localhost

--- utils/model.py
+++ utils/model.py
@@ -1,10 +1,12 @@
 import os
-
+import tensorflow as tf
 
 def save_model(nn_model, path, name) -> None:
 
     if not os.path.exists(path):
         os.makedirs(path)
-
+    
+    save_option = tf.saved_model.SaveOptions(experimental_io_device="/job:localhost")
+    
     fn = os.path.join(path, name)
-    nn_model.save(fn)
+    nn_model.save(fn, options=save_option)
