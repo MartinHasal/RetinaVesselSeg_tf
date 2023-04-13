dbb247f843c02e17ccdf0583f6ba85a83146365d
MartinHasal <m.hasal@email.cz>
2023-04-13 12:31:14 +0200

    removed experimental_io_device=/job:localhost

--- utils/model.py
+++ utils/model.py
@@ -6,7 +6,7 @@ def save_model(nn_model, path, name) -> None:
     if not os.path.exists(path):
         os.makedirs(path)
     
-    save_option = tf.saved_model.SaveOptions(experimental_io_device="/job:localhost")
+    #save_option = tf.saved_model.SaveOptions(experimental_io_device="/job:localhost")
     
     fn = os.path.join(path, name)
-    nn_model.save(fn, options=save_option)
+    nn_model.save(fn) # , options=save_option)
